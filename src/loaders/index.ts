f02ef82374665e6fcda1ff2a5e5a02e37609734c
Valery Yafremau <valeryyafremau@gmail.com>
2022-03-18 15:16:27 +0400

    mertics removed

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -10,50 +10,23 @@ import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 const helmet = require('helmet');
 
-const client = require('prom-client');
-const register = new client.Registry();
+// const client = require('prom-client');
+// const register = new client.Registry();
 
 // Probe every 5th second.
-const intervalCollector = client.collectDefaultMetrics({
+/*const intervalCollector = client.collectDefaultMetrics({
   prefix: 'node_',
   timeout: 5000,
   register
-});
+});*/
 
-const counter = new client.Counter({
-  name: 'node_my_counter',
-  help: 'This is my counter'
-});
-
-const gauge = new client.Gauge({
-  name: 'node_my_gauge',
-  help: 'This is my gauge'
-});
-
-const histogram = new client.Histogram({
-  name: 'node_my_histogram',
-  help: 'This is my histogram',
-  buckets: [0.1, 5, 15, 50, 100, 500]
-});
-
-const summary = new client.Summary({
-  name: 'node_my_summary',
-  help: 'This is my summary',
-  percentiles: [0.01, 0.05, 0.5, 0.9, 0.95, 0.99, 0.999]
-});
-
-register.registerMetric(counter);
-register.registerMetric(gauge);
-register.registerMetric(histogram);
-register.registerMetric(summary);
-
-const httpRequestTimer = new client.Histogram({
+/*const httpRequestTimer = new client.Histogram({
   name: 'http_request_duration_seconds',
   help: 'Duration of HTTP requests in seconds',
   labelNames: ['method', 'route', 'code'],
   buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10] // 0.1 to 10 seconds
 });
-register.registerMetric(httpRequestTimer);
+register.registerMetric(httpRequestTimer);*/
 
 async function mainLoader(app: any) {
   app.enable('trust proxy');
@@ -85,7 +58,7 @@ async function mainLoader(app: any) {
   app.options('*', cors());
 
   app.use('/metrics', async (req: any, res: any) =>
-    res.end(await register.metrics())
+    res.end(/*await register.metrics()*/)
   );
 }
 
